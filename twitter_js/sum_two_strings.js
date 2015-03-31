3c37b7115e6b6164ae54e9d66539cd91dcea6a0a
Alonso Vidales <alonso.vidales@tras2.es>
2015-03-31 10:34:08 +0200

    Modified strings sum

--- twitter_js/sum_two_strings.js
+++ twitter_js/sum_two_strings.js
@@ -1,27 +1,35 @@
-// Add two strings as integers without cast them
+// Add two super large strings as integers without cast them
 
 var IntegersHandler = (function() {
     var my = {};
-    var zeroCode = "0".charCodeAt(0);
-    
+
     my.addStrings = function(str1, str2) {
-        var i = 0, result = 0;
+        var i = 0, sumResultInt = 0;
+        var result = "", sumResult = "";
+        var carry = false;
         var str1Len = str1.length, str2Len = str2.length;
-        
-        for (i = 0; i < str1Len && i < str2Len; i++) {
-            result += (str1[str1Len-1-i].charCodeAt(0) - zeroCode + str2[str2Len-1-i].charCodeAt(0) - zeroCode) * Math.pow(10, i)
+
+        for (i = 0; i < str1Len || i < str2Len; i++) {
+            if (i >= str1Len) {
+                sumResultInt = parseInt(str2[str2Len-1-i]);
+            } else if (i >= str2Len) {
+                sumResultInt = parseInt(str2[str1Len-1-i]);
+            } else {
+                console.log(i, parseInt(str1[str1Len-1-i]));
+                sumResultInt = parseInt(str1[str1Len-1-i]) + parseInt(str2[str2Len-1-i]);
+            }
+            if (carry) {
+                sumResultInt++;
+            }
+            sumResult = String(sumResultInt);
+            result = sumResult[sumResult.length-1] + result;
+            carry = sumResult.length === 2;
         }
         
-        if (i === str1Len) {
-            for (; i < str2Len; i++) {
-                result += (str2[str2Len-1-i].charCodeAt(0) - zeroCode) * Math.pow(10, i)
-            }
-        } else {
-            for (; i < str1Len; i++) {
-                result += (str1[str1Len-1-i].charCodeAt(0) - zeroCode) * Math.pow(10, i)
-            }
+        if (carry) {
+            result = "1" + result;
         }
-        
+
         return result;
     };
     
